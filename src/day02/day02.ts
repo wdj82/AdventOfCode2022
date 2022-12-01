6bc971791251a95b0842c3199e19c7a05bda443a
William <wjones@clearc2.com>
2022-12-01 09:46:28 -0600

    day2 setup

---
+++ src/day02/day02.ts
@@ -0,0 +1,9 @@
+// Advent of Code day 2
+// https://adventofcode.com/2022/day/2
+
+// import rawInput from "./rawInput";
+
+const partOne = "partOne";
+const partTwo = "partTwo";
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
