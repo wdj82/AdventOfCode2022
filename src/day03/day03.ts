1196067c8efd73ca106e0030597765c44c9cc263
William Jones <37749088+wdj82@users.noreply.github.com>
2022-12-03 13:12:13 -0600

    day 3 complete

--- src/day03/day03.ts
+++ src/day03/day03.ts
@@ -1,9 +1,43 @@
 // Advent of Code day 3
 // https://adventofcode.com/2022/day/3
 
-import rawInput from "./rawInput";
+import { rawInput, alphaScores } from "./rawInput";
 
-const partOne = "partOne";
-const partTwo = "partTwo";
+const rucksacks = rawInput.split("\n");
+
+const compartments = rucksacks.map((pack) => [
+  pack.slice(0, pack.length / 2).split(""),
+  pack.slice(pack.length / 2).split(""),
+]);
+
+const partOne = compartments.reduce((acc, [compartment1, compartment2]) => {
+  let item = "";
+  for (let i = 0; i < compartment1.length; i++) {
+    item = compartment1[i];
+    if (compartment2.includes(item)) {
+      break;
+    }
+  }
+  return acc + alphaScores[item];
+}, 0);
+
+let partTwo = 0;
+for (let i = 0; i < rucksacks.length; i += 3) {
+  const elf1 = rucksacks[i].split("");
+  const elf2 = rucksacks[i + 1].split("");
+  const elf3 = rucksacks[i + 2].split("");
+
+  let item = "";
+  for (let j = 0; j < elf1.length; j++) {
+    item = elf1[j];
+    if (elf2.includes(item) && elf3.includes(item)) {
+      break;
+    }
+  }
+
+  partTwo += alphaScores[item];
+}
+
 document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
 document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
