f602063cdeb5b1359c4a331a80fd3951ba3d8565
William Jones <37749088+wdj82@users.noreply.github.com>
2022-12-03 13:12:13 -0600

    day 3 complete

---
+++ src/day04/day04.ts
@@ -0,0 +1,10 @@
+// Advent of Code day 4
+// https://adventofcode.com/2022/day/4
+
+import { testInput, rawInput } from "./rawInput";
+
+const partOne = "partOne";
+const partTwo = "partTwo";
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
