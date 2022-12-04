e429a58ccf1999d22a9f46fd6f4706b0e889cf4d
William Jones <37749088+wdj82@users.noreply.github.com>
2022-12-04 01:18:01 -0600

    day 4 complete

--- src/day04/day04.ts
+++ src/day04/day04.ts
@@ -1,10 +1,25 @@
 // Advent of Code day 4
 // https://adventofcode.com/2022/day/4
 
-import { testInput, rawInput } from "./rawInput";
+import { rawInput } from "./rawInput";
 
-const partOne = "partOne";
-const partTwo = "partTwo";
+const pairs = rawInput.split("\n").map((elf) => elf.split(",").map((sections) => sections.split("-").map(Number)));
+
+let partOne = 0;
+let partTwo = 0;
+pairs.forEach((pair) => {
+  const [elfOneStart, elfOneEnd] = pair[0];
+  const [elfTwoStart, elfTwoEnd] = pair[1];
+  if (
+    (elfOneStart <= elfTwoStart && elfOneEnd >= elfTwoEnd) ||
+    (elfTwoStart <= elfOneStart && elfTwoEnd >= elfOneEnd)
+  ) {
+    partOne += 1;
+  }
+
+  if (Math.max(elfOneStart, elfTwoStart) <= Math.min(elfOneEnd, elfTwoEnd)) partTwo += 1;
+});
+
 document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
 document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
 
