55d570eb4e36e4561283ca985036e31f7658d71e
William <wjones@clearc2.com>
2022-12-05 10:32:22 -0600

    day 5 part 2

--- src/day05/day05.ts
+++ src/day05/day05.ts
@@ -1,38 +1,40 @@
 // Advent of Code day 5
 // https://adventofcode.com/2022/day/5
 
-import { testInput, rawInput } from "./rawInput";
+import { rawInput } from "./rawInput";
 
 const [rawStacks, instructions] = rawInput.split("\n\n").map((lines) => lines.split("\n"));
-
 const numOfStacks = rawStacks.pop()?.trim().split("  ").length;
-const stacks: string[][] = Array.from(Array(numOfStacks), () => new Array(0));
 
-console.log({ rawStacks });
-rawStacks.forEach((stackRow) => {
-  let currStackNum = 0;
-  // console.log({ stackRow });
-  for (let i = 0; i < stackRow.length; i++) {
-    // console.log({ i });
-    const currChar = stackRow[i];
-    // console.log({ currChar });
-    if (currChar === " ") {
-      currStackNum += 1;
-      i += 3;
-      continue;
+function createStacks() {
+  const stacks: string[][] = Array.from(Array(numOfStacks), () => new Array(0));
+  console.log({ rawStacks });
+  rawStacks.forEach((stackRow) => {
+    let currStackNum = 0;
+    // console.log({ stackRow });
+    for (let i = 0; i < stackRow.length; i++) {
+      // console.log({ i });
+      const currChar = stackRow[i];
+      // console.log({ currChar });
+      if (currChar === " ") {
+        currStackNum += 1;
+        i += 3;
+        continue;
+      }
+      if (currChar !== "[") {
+        // console.log({ currStackNum });
+        stacks[currStackNum].unshift(currChar);
+        // console.log(stacks[currStackNum]);
+        currStackNum += 1;
+        i += 3;
+      }
     }
-    if (currChar !== "[") {
-      // console.log({ currStackNum });
-      stacks[currStackNum].unshift(currChar);
-      // console.log(stacks[currStackNum]);
-      currStackNum += 1;
-      i += 3;
-    }
-  }
-});
+  });
+  return stacks;
+}
 
 // console.log({ stacks });
-
+const partOneStack = createStacks();
 instructions.forEach((line) => {
   const [amount, move] = line.split(" from ");
   const numberToMove = Number(amount.split(" ")[1]);
@@ -41,19 +43,40 @@ instructions.forEach((line) => {
   // console.log({ numberToMove, from, to });
 
   for (let i = 0; i < numberToMove; i++) {
-    const box = stacks[from].pop();
+    const box = partOneStack[from].pop();
     // console.log({ box });
-    if (box) stacks[to].push(box);
+    if (box) partOneStack[to].push(box);
   }
-  // console.log("fromStack: ", stacks[from]);
-  // console.log("toStack: ", stacks[to]);
+  // console.log("fromStack: ", partOneStack[from]);
+  // console.log("toStack: ", partOneStack[to]);
 });
 
-const partOne = stacks.reduce((acc, curr) => {
+const partOne = partOneStack.reduce((acc, curr) => {
+  return (acc += curr.at(-1));
+}, "");
+
+const partTwoStack = createStacks();
+instructions.forEach((line) => {
+  const [amount, move] = line.split(" from ");
+  const numberToMove = Number(amount.split(" ")[1]);
+  const [from, to] = move.split(" to ").map((x) => Number(x) - 1);
+
+  // console.log({ numberToMove, from, to });
+
+  const boxes = partTwoStack[from].splice(-numberToMove);
+  // console.log({ boxes });
+  partTwoStack[to] = [...partTwoStack[to], ...boxes];
+
+  // console.log("fromStack: ", partTwoStack[from]);
+  // console.log("toStack: ", partTwoStack[to]);
+});
+
+const partTwo = partTwoStack.reduce((acc, curr) => {
   return (acc += curr.at(-1));
 }, "");
 
 console.log({ partOne });
-const partTwo = "partTwo";
+console.log({ partTwo });
+
 // document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
 document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
