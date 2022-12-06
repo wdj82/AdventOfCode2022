94e2a9c9aeee776239ab46585e1c86c8229477ed
William <wjones@clearc2.com>
2022-12-06 09:05:01 -0600

    day 6 complete

--- src/day06/day06.ts
+++ src/day06/day06.ts
@@ -1,9 +1,28 @@
 // Advent of Code day 6
 // https://adventofcode.com/2022/day/6
 
-import { testInput, rawInput } from "./rawInput";
+import { rawInput } from "./rawInput";
 
-const partOne = "partOne";
-const partTwo = "partTwo";
+const input = rawInput.split("");
+
+function findPacket(length: number) {
+  for (let i = 0; i < input.length; i++) {
+    let marker = input[i];
+
+    for (let j = i + 1; j < i + length; j++) {
+      const nextChar = input[j];
+      if (!marker.includes(nextChar)) {
+        marker += nextChar;
+        if (marker.length === length) {
+          return j + 1;
+        }
+      }
+    }
+  }
+  return -1;
+}
+const partOne = findPacket(4);
+const partTwo = findPacket(14);
+
 document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
 document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
