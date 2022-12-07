c680230d2a31004ff9994c9d2b4b92a69815dfc9
William <wjones@clearc2.com>
2022-12-07 10:44:43 -0600

    day 9 setup

---
+++ src/day09/day09.ts
@@ -0,0 +1,9 @@
+// Advent of Code day 9
+// https://adventofcode.com/2022/day/9
+
+import { testInput, rawInput } from "./rawInput";
+
+const partOne = "partOne";
+const partTwo = "partTwo";
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
