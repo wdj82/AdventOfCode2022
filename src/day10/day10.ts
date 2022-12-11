8bb79b65c9f7736046d4a8466feb318b2fed3e7e
William Jones <37749088+wdj82@users.noreply.github.com>
2022-12-11 11:52:48 -0600

    day 10 complete

--- src/day10/day10.ts
+++ src/day10/day10.ts
@@ -1,9 +1,69 @@
 // Advent of Code day 10
 // https://adventofcode.com/2022/day/10
 
-import { testInput, rawInput } from "./rawInput";
+import { drawToCanvas } from "./drawToCanvas";
+import { rawInput } from "./rawInput";
 
-const partOne = "partOne";
-const partTwo = "partTwo";
+const screen = [...Array(6)].map(() => new Array(40).fill("."));
+
+function drawSprite(registerX: number, currentCycle: number) {
+  // only filling in the crt index based on the current cpu cycle
+  let crtIndex = currentCycle - 1;
+
+  const crtRow = Math.floor(crtIndex / 40);
+  crtIndex = crtIndex % 40;
+  registerX = registerX % 40;
+
+  const sprite = [registerX - 1, registerX, registerX + 1];
+
+  // turn on the pixel if the 3 pixel wide sprite is there
+  if (sprite.includes(crtIndex)) {
+    screen[crtRow][crtIndex] = "#";
+  }
+}
+
+function replaceCRT() {
+  const lines = rawInput.split("\n");
+
+  let registerX = 1;
+  let currentCycle = 1;
+  let currentInputIndex = 0;
+  let toBeAdded: number | null = null;
+  let cycleToCheck = 20;
+  let signalStrength = 0;
+
+  while (currentInputIndex < lines.length) {
+    // for part one
+    if (currentCycle === cycleToCheck) {
+      cycleToCheck += 40;
+      signalStrength += currentCycle * registerX;
+    }
+
+    // for part two
+    drawSprite(registerX, currentCycle);
+
+    if (toBeAdded) {
+      registerX += toBeAdded;
+      toBeAdded = null;
+      // only go to next instruction on completion of current instruction
+      currentInputIndex += 1;
+    } else {
+      const input = lines[currentInputIndex];
+      if (input === "noop") {
+        // noop finishes in one cycle
+        currentInputIndex += 1;
+      } else {
+        // next cycle the add will finish
+        toBeAdded = Number(input.split(" ")[1]);
+      }
+    }
+    currentCycle += 1;
+  }
+
+  return signalStrength;
+}
+
+const partOne = replaceCRT();
 document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
-document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+drawToCanvas(screen);
+
---
+++ src/day12/day12.ts
@@ -0,0 +1,10 @@
+// Advent of Code day 12
+// https://adventofcode.com/2022/day/12
+
+import { testInput, rawInput } from "./rawInput";
+
+const partOne = "partOne";
+const partTwo = "partTwo";
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
