9934c90a58f7aa3af0cde552a0f7372cb4089672
William <wjones@clearc2.com>
2022-12-13 15:01:34 -0600

    day 13 complete

--- src/day13/day13.ts
+++ src/day13/day13.ts
@@ -1,9 +1,7 @@
 // Advent of Code day 13
 // https://adventofcode.com/2022/day/13
 
-import { testInput, rawInput } from "./rawInput";
-
-const pairs = rawInput.split("\n\n");
+import { rawInput } from "./rawInput";
 
 type List = number | List[];
 
@@ -25,42 +23,55 @@ function makeList(array: (number | "[" | "]")[], currentIndex = 0): { newList: L
   return { newList: stack, newIndex: currentIndex };
 }
 
-function checkLists(left: List, right: List): number | boolean {
+function compareLists(left: List, right: List): -1 | 0 | 1 {
   if (Array.isArray(left) && Array.isArray(right)) {
     const maxLength = Math.max(left.length, right.length);
     for (let i = 0; i < maxLength; i++) {
-      if (left[i] === undefined) return true;
-      if (right[i] === undefined) return false;
+      if (left[i] === undefined) return -1;
+      if (right[i] === undefined) return 1;
 
-      const result = checkLists(left[i], right[i]);
+      const result = compareLists(left[i], right[i]);
       if (result !== 0) return result;
     }
     return 0;
   } else if (!Array.isArray(left) && !Array.isArray(right)) {
-    if (left < right) return true;
-    if (left > right) return false;
+    if (left < right) return -1;
+    if (left > right) return 1;
     return 0;
   } else {
     if (Array.isArray(left)) {
-      return checkLists(left, [right]);
+      return compareLists(left, [right]);
     }
-    return checkLists([left], right);
+    return compareLists([left], right);
   }
 }
 
-const partOne = pairs.reduce((acc, pair, index) => {
+const partOne = rawInput.split("\n\n").reduce((acc, pair, index) => {
   const [leftArray, rightArray] = pair.split("\n").map((line) => JSON.parse(line));
 
   const { newList: left } = makeList(leftArray);
   const { newList: right } = makeList(rightArray);
 
-  const result = checkLists(left, right);
-  if (result) return index + 1 + acc;
+  const result = compareLists(left, right);
+  if (result === -1) return index + 1 + acc;
 
   return acc;
 }, 0);
 
-const partTwo = "partTwo";
+const partTwo = rawInput
+  .concat("\n\n", "[[2]]", "\n", "[[6]]")
+  .replace(/\n{2,}/g, "\n")
+  .split("\n")
+  .map((line) => JSON.parse(line))
+  .sort((a, b) => {
+    const { newList: left } = makeList(a);
+    const { newList: right } = makeList(b);
+    return compareLists(left, right);
+  })
+  .reduce((acc, packet, index) => {
+    if (JSON.stringify(packet) === "[[2]]" || JSON.stringify(packet) === "[[6]]") return (index + 1) * acc;
+    return acc;
+  }, 1);
 
 console.log({ partOne });
 console.log({ partTwo });
